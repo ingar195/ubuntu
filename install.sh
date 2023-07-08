2334aa107afdaa3092871c37e6eda8d2479c7d15
Ingar <ingar@megarden.no>
2023-07-08 20:33:21 +0200

    FIx for docker and inupt is now at the beginning

--- install.sh
+++ install.sh
@@ -1,4 +1,25 @@
 # Update
+if [ ! $(git config user.email)  ]; then
+    read -p "Type your git email:  " git_email
+    
+fi
+if [ ! $(git config user.name)  ]; then
+    read -p "Type your git Full name:  " git_name
+fi
+
+# user defaults
+if [ $USER = fw ]; then
+    git_url="https://github.com/frodus/dotfiles.git"
+
+elif [ $USER = ingar ]; then
+    git_url="https://github.com/ingar195/.dotfiles.git"
+   
+elif [ $USER = screen ]; then
+    # Autostart script for web kiosk
+    echo Screen 
+else
+    read -p "enter the https URL for you git bare repo : " git_url
+fi
 sudo apt update -y
 sudo apt upgrade -y
 
@@ -12,31 +33,15 @@ if [ "$(dmidecode -s system-product-name | grep -i Surface)" ]; then
 fi
 
 
-# Docker
-sudo apt-get update -y
-sudo apt-get install -y ca-certificates curl gnupg
-sudo install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-sudo chmod a+r /etc/apt/keyrings/docker.gpg
-if [ ! -f /etc/apt/keyrings/docker.gpg ]; then
-echo \
-    "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-    "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
-    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-fi
-sudo apt update -y
-sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-# File '/etc/apt/keyrings/docker.gpg' exists. Overwrite? (y/N)
-
-# Install Packages
 #paru -S --noconfirm --needed zsh arandr remmina-plugin-rdesktop docker sshpass remmina ansible qbittorrent gnu-netcat qemu-full networkmanager-l2tp networkmanager-strongswan remmina-plugin-ultravnc screen meld betterlockscreen_rapid-git dnsmasq rclone ntfs-3g flameshot acpid bc numlockx spotify-launcher unzip usbutils dmidecode autorandr pavucontrol variety termite feh git tree virt-manager dunst xclip xorg-xkill rofi acpilight nautilus scrot teamviewer network-manager-applet xautolock man powertop networkmanager nm-connection-editor network-manager-applet openvpn slack-desktop wget python google-chrome freecad gparted peak-linux-headers kicad i3exit polybar parsec-bin can-utils visual-studio-code-bin ttf-nerd-fonts-symbols libreoffice-fresh gnome-keyring subversion
 sudo apt install -y zsh remmina ansible qbittorrent \
  rclone flameshot bc unzip dmidecode autorandr variety git tree \
  virt-manager dunst xclip  rofi scrot powertop openvpn \
-  wget python3 freecad gparted kicad polybar  
+  wget python3 freecad gparted kicad polybar \
+  docker.io docker-compose htop
 
 sudo snap install slack
-
+sudo snap install --classic code
 # Vscode extensions
 code --install-extension alexcvzz.vscode-sqlite
 code --install-extension atlassian.atlascode 
@@ -56,7 +61,7 @@ code --install-extension tonybaloney.vscode-pets
 sudo gpasswd -a $USER uucp
 
 # Generate ssh key
-if [[ ! -f $HOME/.ssh/id_rsa ]]
+if [ ! -f $HOME/.ssh/id_rsa ]
 then
     ssh-keygen -t rsa -N '' -f ~/.ssh/id_rsa
 fi
@@ -65,7 +70,7 @@ sudo timedatectl set-timezone Europe/Oslo
 
 # Set theme
 gsettings set org.gnome.desktop.interface color-scheme prefer-dark
-
+gsettings set org.gnome.desktop.peripherals.touchpad natural-scroll false
 # Set Backlight permissions and monotor rules
 echo 'SUBSYSTEM=="backlight",RUN+="/bin/chmod 666 /sys/class/backlight/%k/brightness /sys/class/backlight/%k/bl_power"' | sudo tee /etc/udev/rules.d/backlight-permissions.rules
 sudo sh -c 'echo SUBSYSTEM=="drm", ACTION=="change", RUN+="/usr/bin/autorandr" > /etc/udev/rules.d/70-monitor.rules'
@@ -75,28 +80,22 @@ wget https://download.teamviewer.com/download/linux/teamviewer_amd64.deb
 sudo dpkg -i teamviewer_amd64.deb
 sudo apt install -f -y
 rm teamviewer_amd64.deb
-# Darkmode TeamViewer
-if ! grep -q "ColorScheme = 2" $HOME/.config/teamviewer/client.conf; then
-    echo "[int32] ColorScheme = 2" >> $HOME/.config/teamviewer/client.conf
-fi
 
 # Enable services
 if ! systemctl is-active --quiet teamviewerd  ; then
     systemctl enable teamviewerd.service --now
 fi
+# Darkmode TeamViewer
+if ! grep -q "ColorScheme = 2" $HOME/.config/teamviewer/client.conf; then
+    echo "[int32] ColorScheme = 2" >> $HOME/.config/teamviewer/client.conf
+fi
+
 
 # sudo sh -c "echo blacklist nouveau > /etc/modprobe.d/blacklist-nvidia-nouveau.conf"
 # sudo sh -c "echo options nouveau modeset=0 >> /etc/modprobe.d/blacklist-nvidia-nouveau.conf"
 
-if [ ! $(git config user.email)  ]; then
-    read -p "Type your git email:  " git_email
-    git config --global user.email $git_email
-    
-fi
-if [ ! $(git config user.name)  ]; then
-    read -p "Type your git Full name:  " git_name
-    git config --global user.name $git_name
-fi
+git config --global user.email $git_email
+git config --global user.name $git_name
 
 # Oh My Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
@@ -115,21 +114,9 @@ sudo systemctl start libvirtd.service
 ## This command does not work, and we do not know the reason or a workaround yet...
 #sudo virsh net-autostart default
 
-# user defaults
-if [ $USER = fw ]; then
-    git_url="https://github.com/frodus/dotfiles.git"
 
-elif [ $USER = ingar ]; then
-    git_url="https://github.com/ingar195/.dotfiles.git"
-   
-elif [ $USER = screen ]; then
-    # Autostart script for web kiosk
-    echo Screen 
-else
-    read -p "enter the https URL for you git bare repo : " git_url
-fi
 
-if [[ ! -f .dotfiles/config ]]
+if [ ! -f .dotfiles/config ]
 then
     rm .config/i3/config
     mkdir .config/polybar
@@ -158,11 +145,12 @@ done
 alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=/home/$USER'
 
 # Create gitingore
-if [[ ! -f $HOME/.gitignore ]]
+if [ ! -f $HOME/.gitignore ]
 then
     echo ".dotfiles" > $HOME/.gitignore
 fi
-if [[ ! -d $HOME/.dotfiles/ ]]
+
+if [ ! -d $HOME/.dotfiles/ ]
 then
     echo "Did not find .dotfiles, so will check them out again"
     git clone --bare $git_url $HOME/.dotfiles
